14592523a124c4bb66f773b4f0a978df7876e314
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-07-02 16:26:05 +0200

    Test fixing api route

--- app/pages/api/files/upload.ts
+++ app/pages/api/files/upload.ts
@@ -9,68 +9,70 @@ import lodash from "lodash";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DigitalOceanSpacesClient } from "../../../src/digitalOceanClient/digitalOceanClient";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const form = new formidable.IncomingForm();
-  const admZip = new AdmZip();
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise((reject, resolve) => {
+    const form = new formidable.IncomingForm();
+    const admZip = new AdmZip();
 
-  form.parse(req, async (err, _, files) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+    form.parse(req, async (err, _, files) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-    const excelFile = files.excelFile;
-    const docxFile = files.docxFile;
+      const excelFile = files.excelFile;
+      const docxFile = files.docxFile;
 
-    // @ts-ignore
-    const rawExcelFile = fs.readFileSync(excelFile.filepath.toString());
-    // @ts-ignore
-    const rawDocxFile = fs.readFileSync(docxFile.filepath.toString());
+      // @ts-ignore
+      const rawExcelFile = fs.readFileSync(excelFile.filepath.toString());
+      // @ts-ignore
+      const rawDocxFile = fs.readFileSync(docxFile.filepath.toString());
 
-    const mapping = excelToJson({
-      source: rawExcelFile,
-      header: {
-        rows: 1,
-      },
-      columnToKey: {
-        "*": "{{columnHeader}}",
-      },
-    })["Sheet1"];
+      const mapping = excelToJson({
+        source: rawExcelFile,
+        header: {
+          rows: 1,
+        },
+        columnToKey: {
+          "*": "{{columnHeader}}",
+        },
+      })["Sheet1"];
 
-    const pizZip = new PizZip(rawDocxFile);
+      const pizZip = new PizZip(rawDocxFile);
 
-    const doc = new Docxtemplater(pizZip, {
-      paragraphLoop: true,
-      linebreaks: true,
-    });
-
-    mapping.forEach((valuesJson, index) => {
-      const buffer = lodash.clone(doc);
-
-      buffer.render(valuesJson);
-
-      const renderedDocx = buffer.getZip().generate({
-        type: "nodebuffer",
-        // compression: DEFLATE adds a compression step.
-        // For a 50MB output document, expect 500ms additional CPU time
-        compression: "DEFLATE",
+      const doc = new Docxtemplater(pizZip, {
+        paragraphLoop: true,
+        linebreaks: true,
       });
 
-      admZip.addFile(`output_${index}.docx`, renderedDocx);
-    });
+      mapping.forEach((valuesJson, index) => {
+        const buffer = lodash.clone(doc);
 
-    const digitalOceanSpacesClient = new DigitalOceanSpacesClient();
+        buffer.render(valuesJson);
 
-    digitalOceanSpacesClient
-      .uploadFile({
-        fileBuffer: admZip.toBuffer(),
-        fileName: "test-file.zip",
-      })
-      .then(({ downloadLink }) => {
-        res.status(201).json({
-          downloadLink,
+        const renderedDocx = buffer.getZip().generate({
+          type: "nodebuffer",
+          // compression: DEFLATE adds a compression step.
+          // For a 50MB output document, expect 500ms additional CPU time
+          compression: "DEFLATE",
         });
+
+        admZip.addFile(`output_${index}.docx`, renderedDocx);
       });
+
+      const digitalOceanSpacesClient = new DigitalOceanSpacesClient();
+
+      digitalOceanSpacesClient
+        .uploadFile({
+          fileBuffer: admZip.toBuffer(),
+          fileName: "test-file.zip",
+        })
+        .then(({ downloadLink }) => {
+          res.status(201).json({
+            downloadLink,
+          });
+        });
+    });
   });
 };
 
