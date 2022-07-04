726cdf8762c09a8767df4b57c3fb22e0e8e5decc
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-07-04 12:12:47 +0200

    Use fork to process file generation

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 /** @type {import("src/initializer")}; */
+const path = require("path");
+const { merge } = require("webpack-merge");
 
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (
+    config,
+    { _buildId, _dev, isServer, _defaultLoaders, _webpack }
+  ) => {
+    if (isServer) {
+      return merge(config, {
+        entry() {
+          return config.entry().then((entry) => {
+            return Object.assign({}, entry, {
+              renderFiles: path.resolve(process.cwd(), "src/renderFiles.ts"),
+            });
+          });
+        },
+      });
+    } else {
+      return config;
+    }
+  },
 };
 
 module.exports = nextConfig;
--- app/src/renderFiles.ts
+++ app/src/renderFiles.ts
@@ -1,14 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { IncomingForm, File } from "formidable";
+import { File } from "formidable";
 import AdmZip from "adm-zip";
 import PizZip from "pizzip";
 import Docxtemplater from "docxtemplater";
 import fs from "fs";
 import excelToJson from "convert-excel-to-json";
 import lodash from "lodash";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { DigitalOceanSpacesClient } from "../../../src/digitalOceanClient/digitalOceanClient";
-import { fork } from "child_process";
+import { DigitalOceanSpacesClient } from "./digitalOceanClient/digitalOceanClient";
 
 const extractMappingValues = (excelFile: File) => {
   const rawExcelFileData = fs.readFileSync(excelFile.filepath.toString());
@@ -87,29 +85,8 @@ const processFileGeneration = async (excelFile: File, docxFile: File) => {
   return downloadLink;
 };
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const form = new IncomingForm();
+process.on("message", async (msg) => {
+  const downloadLink = await processFileGeneration(msg.excelFile, msg.docxFile);
 
-  form.parse(req, async (err, _, files) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    const forked = fork("./.next/server/renderFiles.js");
-
-    forked.send(files);
-
-    forked.on("message", (downloadLink) => {
-      res.status(200).json({
-        downloadLink,
-      });
-    });
-  });
-};
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+  process.send(downloadLink);
+});
