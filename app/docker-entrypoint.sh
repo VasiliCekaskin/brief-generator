60a2e11c9d24acf6b824ff6c382c3d335bbb1848
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-07-05 11:19:16 +0200

    Add prisma to package scripts

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "start" ]; then
+  DATABSE_URL=postgresql://postgres:postgres@localhost/letter-friend npm run prisma migrate deploy
   exec npm run start
 else
   exec "$@"
