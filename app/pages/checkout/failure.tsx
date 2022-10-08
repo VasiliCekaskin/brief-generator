456be7ba22a39d875f0f8d64b51a738f24cc72db
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-10-08 16:19:07 +0200

    Add stripe payment

---
+++ app/pages/checkout/failure.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-key */
+import { NextPage } from "next";
+import React from "react";
+
+const Failure: NextPage = () => {
+  return <>Payment failed.</>;
+};
+
+export default Failure;
---
+++ app/pages/checkout/success.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-key */
+import { NextPage } from "next";
+import React from "react";
+
+const Success: NextPage = () => {
+  return <>Payment successful.</>;
+};
+
+export default Success;
