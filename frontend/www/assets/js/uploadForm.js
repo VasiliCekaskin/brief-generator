2c79978df77fce27452a80cea2e1820f23984982
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-10-11 23:20:05 +0200

    Add form for file upload

--- frontend/www/assets/js/uploadForm.js
+++ frontend/www/assets/js/uploadForm.js
@@ -1,10 +1,19 @@
 (function () {
+  document
+    .getElementById("dropzone-excelFile")
+    .addEventListener("change", (event) => {
+      let fileName = event.target.files[0].name;
+
+      document.getElementById("uploaded-excelFile").innerText = fileName;
+    });
+
   console.log(document.getElementById("dropzone-excelFile"));
 
   document
-    .getElementById("dropzone-excelFile")
-    .addEventListener("focus", (event) => {
-      console.log("wASDADSADSADSADw");
-      event.target.querySelector("span").innerHTML = "BLUBH";
+    .getElementById("dropzone-docsFile")
+    .addEventListener("change", (event) => {
+      let fileName = event.target.files[0].name;
+
+      document.getElementById("uploaded-docsFile").innerText = fileName;
     });
 })();
